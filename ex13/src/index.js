71aeb5c659b335a2e7b2a7a2ed8b706dd5a3b1f8
misja98 <mirko.djukic@icloud.com>
2021-09-21 04:57:34 +0200

    fourteenth finished thirteenth unfinished

---
+++ ex13/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function randomRangeNumber(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random()*(max - min+1)+min);
+
+}
+console.log(randomRangeNumber())
+
+    // Only change code above this line
+    module.exports = randomRangeNumber;
